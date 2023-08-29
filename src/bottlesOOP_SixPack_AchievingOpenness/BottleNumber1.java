163a13d8c1b58c295db0a8dc07ce298c7ef1ec27
Dan Seiter <Dan.Seiter@neosoftmedical.com>
2023-08-29 15:29:34 -0500

    Separated conditionals into classes to leverage polymorphism. Added simple factory for choosing proper class based on number

---
+++ src/bottlesOOP_SixPack_AchievingOpenness/BottleNumber1.java
@@ -0,0 +1,18 @@
+package bottlesOOP_SixPack_AchievingOpenness;
+
+public class BottleNumber1 extends BottleNumber{
+
+	public BottleNumber1(int number) {
+		super(number);
+	}
+	
+    @Override
+    public String container() {
+    		return "bottle";
+    }
+    
+    @Override
+    public String pronoun() {
+    		return "it";
+    }
+}
